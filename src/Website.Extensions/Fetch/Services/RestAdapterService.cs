756155a6261037586ff6164b400ac0df0952b566
Christian Arensbak <christian.arensbak@twoday.com>
2024-11-06 10:23:51 +0100

    added fetch logic back.

---
+++ src/Website.Extensions/Fetch/Services/RestAdapterService.cs
@@ -0,0 +1,79 @@
+using Microsoft.Extensions.Logging;
+using System.Text.Json;
+using Umbraco.Cms.Core.Models;
+using Umbraco.Cms.Core.Services;
+using Website.Extensions.Fetch.Models;
+
+namespace Website.Extensions.Fetch.Services;
+public class RestAdapterService : IAdapterService<SeriesModel, string> {
+  private readonly IContentService _contentService;
+  private readonly ILogger<RestAdapterService> _loggerService;
+  private readonly NodeInitializerService _nodeInitializerService;
+  private readonly IHttpClientFactory _httpClientFactory;
+  private IContent? _lastSeries = null;
+  private List<IContent> _sizeChildren = [];
+
+  public RestAdapterService(IContentService contentService, ILogger<RestAdapterService> loggerService,
+    NodeInitializerService nodeInitializerService, IHttpClientFactory httpClientFactory) {
+    _contentService = contentService;
+    _loggerService = loggerService;
+    _nodeInitializerService = nodeInitializerService;
+    _httpClientFactory = httpClientFactory;
+  }
+
+  public async Task CreateNodesFromSource() {
+    List<SeriesModel> shoeSeries = await FetchFromSource("http://IP_ADDRESS_HERE:8080/api/shoe/getallshoes"); //TODO: Environment Variables
+    foreach (SeriesModel entry in shoeSeries) {
+      IContent seriesNode = _nodeInitializerService.InitializeSeries(entry.Name);
+      seriesNode.SetValue("brand", entry.Brand);
+      seriesNode.SetValue("price", entry.Price);
+      _loggerService.LogInformation($"ITERATING Series {seriesNode}");
+
+      foreach (SizeModel size in entry.Sizes) {
+        IContent sizeNode = _contentService.Create($"Size {size.Size}", seriesNode, "productSize");
+        sizeNode.SetValue("sku", size.SKU);
+        sizeNode.SetValue("stock", size.Stock);
+      }
+    }
+  }
+
+  public async Task<List<SeriesModel>> FetchFromSource(string source) {
+    HttpClient httpClient = _httpClientFactory.CreateClient();
+    string formattedUrl = GetFormattedUrl(source);
+    httpClient.BaseAddress = new Uri(formattedUrl);
+    httpClient.DefaultRequestHeaders.Add("username", "Hovedopgave"); //TODO: Environment Variables
+    httpClient.DefaultRequestHeaders.Add("password", "TtXr@oA7M.PaNxCmsvL2WBMw");  //TODO: Environment Variables
+    httpClient.Timeout = TimeSpan.FromMinutes(1);
+
+    HttpResponseMessage response = await httpClient.GetAsync(source);
+
+    JsonSerializerOptions options = new() {
+      PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+    };
+    var seriesList = new List<SeriesModel>();
+
+    try {
+      seriesList = JsonSerializer.Deserialize<List<SeriesModel>>(response.Content.ReadAsStream(), options);
+    } catch (Exception ex) { } //TODO: Exception handling - Prolly some logging stuff
+
+    if (seriesList is null) {
+      seriesList = [];
+      //TODO: DO SOME LOGGING HERE
+    }
+    return seriesList;
+  }
+
+  // The HttpClient.BaseAddress Uri has to have some specific criteria met, otherwise it will throw an exception.
+  // It must begin with "https://" or "http://" and end with a "/".
+  // https://learn.microsoft.com/en-us/dotnet/api/system.uri?view=net-8.0
+  private static string GetFormattedUrl(string url) {
+    if (!url.StartsWith("https://") && !url.StartsWith("http://")) {
+      url = "https://" + url;
+    }
+    if (!url.EndsWith('/')) {
+      url = url + "/";
+    }
+
+    return url;
+  }
+}
---
+++ src/Website.Extensions/Hangfire/Startup/HangfireComposer.cs
@@ -0,0 +1,75 @@
+﻿using Hangfire;
+using Hangfire.Console;
+using Hangfire.SqlServer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Umbraco.Cms.Core.Composing;
+using Umbraco.Cms.Core.DependencyInjection;
+using Umbraco.Cms.Web.BackOffice.Authorization;
+using Umbraco.Cms.Web.Common.ApplicationBuilder;
+using Website.Extensions.Hangfire.Extensions;
+using Constants = Umbraco.Cms.Core.Constants;
+
+namespace Website.Extensions.Hangfire.Startup;
+public sealed class HangfireComposer : IComposer {
+  public void Compose( IUmbracoBuilder builder ) {
+    // Configure Hangfire to use our current database and add the option to write console messages
+    string? connectionString = builder.Config.GetConnectionString( "localExternalDB" );
+
+    if ( string.IsNullOrEmpty( connectionString ) ) {
+      connectionString = builder.Config.GetConnectionString( Constants.System.UmbracoConnectionName );
+    }
+
+    if ( !string.IsNullOrEmpty( connectionString ) ) {
+      builder.Services.AddHangfire( configuration => {
+        configuration
+            .SetDataCompatibilityLevel( CompatibilityLevel.Version_170 )
+            .UseSimpleAssemblyNameTypeSerializer()
+            .UseRecommendedSerializerSettings()
+            .UseConsole()
+            .UseSqlServerStorage( connectionString, new SqlServerStorageOptions {
+              CommandBatchMaxTimeout = TimeSpan.FromMinutes( 5 ),
+              SlidingInvisibilityTimeout = TimeSpan.FromMinutes( 5 ),
+              QueuePollInterval = TimeSpan.Zero,
+              UseRecommendedIsolationLevel = true,
+              DisableGlobalLocks = true,
+            } );
+      } );
+
+      // Run the required server so your queued jobs will get executed
+      builder.Services.AddHangfireServer();
+
+      AddAuthorizedUmbracoDashboard( builder );
+
+      // For some reason we need to give it the connection string again, else we get this error:
+      // https://discuss.hangfire.io/t/jobstorage-current-property-value-has-not-been-initialized/884
+      JobStorage.Current = new SqlServerStorage( connectionString );
+      JobStorage.Current.PurgeJobs();
+      JobStorage.Current.PurgeRecurringJobs();
+
+    }
+  }
+
+  private static void AddAuthorizedUmbracoDashboard( IUmbracoBuilder builder ) {
+    // Add a named policy to authorize requests to the dashboard
+    builder.Services.AddAuthorizationBuilder().AddPolicy( HangfireConstants.HangfireDashboard, policy => {
+        // We require a logged in backoffice user who has access to the settings section
+        policy.AuthenticationSchemes.Add( Constants.Security.BackOfficeAuthenticationType );
+        policy.Requirements.Add( new SectionRequirement( Constants.Applications.Settings ) );
+      } );
+
+    // Add the dashboard and make sure it's authorized with the named policy above
+    builder.Services.Configure<UmbracoPipelineOptions>( options => {
+      options.AddFilter( new UmbracoPipelineFilter( HangfireConstants.HangfireDashboard ) {
+        Endpoints = app => app.UseEndpoints( endpoints => {
+          endpoints.MapHangfireDashboard(
+                  pattern: "/umbraco/backoffice/hangfire",
+                  options: new DashboardOptions() {
+                    Authorization = new[] { new UmbracoAuthorizationFilter() }
+                  } ).RequireAuthorization( HangfireConstants.HangfireDashboard );
+        } ).UseHangfireDashboard()
+      } );
+    } );
+  }
+}
