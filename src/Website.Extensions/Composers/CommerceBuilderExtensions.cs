b49a303007f5211b2f53205e99429091d75139b8
Christian Arensbak <christian.arensbak@twoday.com>
2024-11-15 20:54:17 +0100

    test: Added very basic eventhandler that should be able to be hit by a breakpoint.

---
+++ src/Website.Extensions/Composers/CommerceBuilderExtensions.cs
@@ -0,0 +1,14 @@
+﻿using SoleMates.Website.Extensions.EventHandlers;
+using Umbraco.Commerce.Core;
+using Umbraco.Commerce.Core.Events.Notification;
+using Umbraco.Commerce.Extensions;
+
+namespace SoleMates.Website.Extensions.Composers;
+public static class CommerceBuilderExtensions {
+  public static IUmbracoCommerceBuilder AddEventListeners(IUmbracoCommerceBuilder builder) {
+    builder.WithNotificationEvent<OrderFinalizedNotification>()
+      .RegisterHandler<OrderCreatedHandler>();
+
+    return builder;
+  }
+}
