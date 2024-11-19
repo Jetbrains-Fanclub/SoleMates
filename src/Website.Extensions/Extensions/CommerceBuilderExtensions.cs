bb831be99a6c0c9b553d29a8c2e7eac52956a197
Christian Arensbak <christian.arensbak@twoday.com>
2024-11-19 19:33:54 +0100

    feat: Now updates the ERP stock when an order completes.

---
+++ src/Website.Extensions/Extensions/CommerceBuilderExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SoleMates.Website.Extensions.EventHandlers;
+using SoleMates.Website.Extensions.Sync.Adapters;
+using Umbraco.Commerce.Core;
+using Umbraco.Commerce.Core.Events.Notification;
+using Umbraco.Commerce.Extensions;
+
+namespace SoleMates.Website.Extensions.Extensions;
+public static class CommerceBuilderExtensions {
+  public static IUmbracoCommerceBuilder AddCommerceEventListeners(this IUmbracoCommerceBuilder builder) {
+    builder.Services.AddSingleton<RestAdapter>();
+    builder.WithNotificationEvent<OrderFinalizedNotification>()
+      .RegisterHandler<OrderFinalizedHandler>();
+
+    return builder;
+  }
+}
