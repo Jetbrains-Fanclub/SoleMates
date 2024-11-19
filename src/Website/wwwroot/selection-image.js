7a7beecf391680144f49505596c35701a97aa2c7
Benjamin F. Stigsen <benjamin.f.stigsen@twoday.com>
2024-11-19 21:29:45 +0100

    add image selection and preview images to product page

---
+++ src/Website/wwwroot/selection-image.js
@@ -0,0 +1,19 @@
+﻿let preview = document.getElementById("preview");
+let images = document.getElementsByClassName("selection-image");
+
+for (let image of images) {
+  image.addEventListener('click', (event) => {
+    let target = event.target;
+    if (target.classList.contains("active")) {
+      return;
+    }
+
+    for (let element of images) {
+      element.classList.remove("active");
+    }
+
+    target.classList.add("active");
+
+    preview.setAttribute("src", target.getAttribute("src"));
+  });
+}
